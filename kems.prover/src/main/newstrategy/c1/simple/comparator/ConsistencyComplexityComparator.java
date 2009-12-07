f21a66059aa1a0e182cca229dcbaeb5d5e08d907
Adolfo Neto <adolfo.usp@gmail.com>
2009-12-07 10:58:18 -0200

    Some changes in comments for ConsistencyComplexityComparator

--- kems.prover/src/main/newstrategy/c1/simple/comparator/ConsistencyComplexityComparator.java
+++ kems.prover/src/main/newstrategy/c1/simple/comparator/ConsistencyComplexityComparator.java
@@ -10,9 +10,8 @@ import main.newstrategy.cpl.configurable.comparator.ISignedFormulaComparator;
  * A signed formula comparator that gives preference to formulas whose
  * complexity degree is higher.
  * 
- * Limitation: only atomic formulas whose size as string is less than 2
- * 
- * @author Emerson Shigueo Sugimoto
+ * @author Emerson Shigueo Sugimoto (original version)
+ * @author Adolfo Neto (revisions)
  * 
  */
 public class ConsistencyComplexityComparator implements
@@ -34,20 +33,15 @@ public class ConsistencyComplexityComparator implements
 	 */
 	public int compare(SignedFormula sf0, SignedFormula sf1) {
 
-		int rt = 0;
-
 		if (getConsistencyDegree(sf0) == getConsistencyDegree(sf1)) {
-			rt = 0;
+			return 0;
 		} else {
 			if (getConsistencyDegree(sf0) > getConsistencyDegree(sf1)) {
-				rt = 1;
+				return 1;
 			} else {
-				rt = -1;
+				return -1;
 			}
 		}
-
-		return rt;
-
 	}
 
 	/*
@@ -75,16 +69,16 @@ public class ConsistencyComplexityComparator implements
 	}
 
 	/**
-	 * Retorna true se por exemplo para uma fórmula: !(X&!Y) se X e Y são
-	 * iguais, como em !(A&!A), e falso se X e Y são diferentes, como em:
-	 * !(A&!B) <br />
-	 * Verifica também fórmulas do tipo: !(A1&!A1)
+	 * Returns "true" when, for a formula whose pattern is !(X&!Y), X and Y are
+	 * the same formula. Returns "false" otherwise.
 	 * */
 	private boolean validFormula(String formula) {
 		int length = formula.length();
 
 		// System.out.println(formula);
-
+		// length - 5 must be an even number. This is true because if one
+		// removes the "(!&!)" characters, in order to have the same string two times,
+		// there must be an even number of characters.
 		if ((length - 5) % 2 == 0) {
 			int middleAndPosition = ((length - 3) / 2);
 			String part0 = formula.substring(2 + middleAndPosition - 1,
@@ -96,27 +90,12 @@ public class ConsistencyComplexityComparator implements
 			String part1 = formula.substring(2, 2 + middleAndPosition - 1);
 			String part2 = formula.substring(2 + middleAndPosition + 1,
 					length - 1);
-
 			// System.out.println(part0 + " - " + part1 + " - " + part2);
-			//
 			// System.out.println(part1.equals(part2));
 			return part1.equals(part2);
-
 		} else {
 			// System.out.println(false);
 			return false;
 		}
-
-		// if (formula.length() == 7) { // !(A&!A)
-		// char X = formula.charAt(2);
-		// char Y = formula.charAt(5);
-		// return X == Y;
-		// } else if (formula.length() == 9) { // !(A1&!A1)
-		// String X = formula.substring(2, 4);
-		// String Y = formula.substring(6, 8);
-		// return X.equals(Y);
-		// } else {
-		// return false;
-		// }
 	}
 }
